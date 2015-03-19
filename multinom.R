a01d46e2653b5df5a1938b7f3a47ced8bffb9078
Matthew Bender <benderm@gmail.com>
2015-03-18 22:56:46 -0600

    NN multinom model
    Score: 0.63520

---
+++ multinom.R
@@ -0,0 +1,16 @@
+#Load dependency
+library(nnet)
+
+# set seed
+set.seed(1337)
+
+#load data
+train<-read.csv("Data/train.csv")[,-1]
+test<-read.csv("Data/test.csv")[,-1]
+submit <- read.csv('Data/sampleSubmission.csv')
+
+# fit and predict
+model <- multinom(target~., train, maxit=666)
+submit[, 2:10] <- predict(model, newdata=test, type='prob')
+
+write.csv(submit,"Output/multinom_2.csv",row.names=FALSE)
