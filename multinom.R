5e2cc1b87a2d9894a1d8ff5c60c8a27bd9c50f05
Matthew Bender <benderm@gmail.com>
2015-03-21 12:45:15 -0600

    up the multinom iterations
    score: 0.63439

--- multinom.R
+++ multinom.R
@@ -10,7 +10,7 @@ test<-read.csv("Data/test.csv")[,-1]
 submit <- read.csv('Data/sampleSubmission.csv')
 
 # fit and predict
-model <- multinom(target~., train, maxit=666)
+model <- multinom(target~., train, maxit=1337)
 submit[, 2:10] <- predict(model, newdata=test, type='prob')
 
-write.csv(submit,"Output/multinom_2.csv",row.names=FALSE)
+write.csv(submit,"Output/multinom_3.csv",row.names=FALSE)
